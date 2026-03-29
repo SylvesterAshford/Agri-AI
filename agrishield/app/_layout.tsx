9454802a163724dcf8acfe187481a0ed6447fbf7
Min Bhone San <darksideofmbs2020@gmail.com>
2026-03-29 16:48:57 +0630

    bug-fixed

--- agrishield/app/_layout.tsx
+++ agrishield/app/_layout.tsx
@@ -24,6 +24,7 @@ function RootLayoutNav() {
   return (
     <Stack screenOptions={{ headerBackTitle: "Back" }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
